2a5285168cb5d5bdde720ab897e230c4b8541ed8
khalid chouhan <khalidchuhan7886@gmail.com>
2026-08-16 19:49:26 +0500

    add express-async-handler; enhance Todo model validation and update controller response format

--- src/controllers/Todo.controller.js
+++ src/controllers/Todo.controller.js
@@ -1,7 +1,11 @@
 import asyncHandler from 'express-async-handler';
-import Todo from '../models/Todo.model';
+import Todo from '../models/Todo.model.js';
 
 export const getTodos = asyncHandler(async (req, res) => {
-    const todos = await Todo.find();
-    res.json(todos);
+    const todos = await Todo.find().sort({ createdAt: -1 });
+    res.status(200).json({
+        success: true,
+        count: todos.length,
+        data: todos,
+    })
 });
--- src/models/Todo.model.js
+++ src/models/Todo.model.js
@@ -3,11 +3,17 @@ import mongoose from 'mongoose';
 const todoSchema = new mongoose.Schema({
     title: {
         type: String,
-        required: true
+        required: [true, "Title is required"],
+        trim: true,
+        maxlength: [100, 'Title cannot exceed 100 characters'],
+
     },
     description: {
         type: String,
-        required: true
+        required: [true, "Description is required"],
+        trim: true,
+        maxlength: [500, 'Description cannot exceed 500 characters'],
+
     },
     completed: {
         type: Boolean,
